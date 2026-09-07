eadd92b9c874b950b2ebfa6432859def43aaa085
Neel Abhijeet Butala <neelbutala@gmail.com>
2026-09-07 17:41:49 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int profit = 0;
+        for(int i = 0; i<prices.size(); i++){
+            profit = max(profit, prices[i] - mini);
+            mini = min(prices[i], mini);
+        }
+
+        return profit;
+    }
+};
